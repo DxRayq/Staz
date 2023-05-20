147db43250fdc9e8914d587e883db42b246f36b3
DxRayq <100144396+DxRayq@users.noreply.github.com>
2023-05-20 20:09:22 +0200

    submit.php

--- submit.php
+++ submit.php
@@ -1,19 +1,21 @@
 <?php
 // Kod przetwarzania formularza...
 
-// Sprawdź weryfikację CAPTCHA
-$recaptcha_secret = "6LfALSUmAAAAADC8lRDky73i1OwU2n_WNGGxSNKy";
-$response = $_POST['g-recaptcha-response'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Sprawdź weryfikację CAPTCHA
+  $recaptcha_secret = "6LfALSUmAAAAADC8lRDky73i1OwU2n_WNGGxSNKy";
+  $response = $_POST['g-recaptcha-response'];
 
-$verify = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$recaptcha_secret}&response={$response}");
-$captcha_success = json_decode($verify);
+  $verify = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$recaptcha_secret}&response={$response}");
+  $captcha_success = json_decode($verify);
 
-if ($captcha_success->success) {
-  // Kod do wykonania, jeśli CAPTCHA została zweryfikowana poprawnie
-  // Przetwarzanie danych formularza...
-  echo "CAPTCHA zweryfikowana. Formularz może być przetworzony.";
-} else {
-  // Kod do wykonania, jeśli CAPTCHA nie została zweryfikowana poprawnie
-  echo "Błąd weryfikacji CAPTCHA. Spróbuj ponownie.";
+  if ($captcha_success->success) {
+    // Kod do wykonania, jeśli CAPTCHA została zweryfikowana poprawnie
+    // Przetwarzanie danych formularza...
+    echo "CAPTCHA zweryfikowana. Formularz może być przetworzony.";
+  } else {
+    // Kod do wykonania, jeśli CAPTCHA nie została zweryfikowana poprawnie
+    echo "Błąd weryfikacji CAPTCHA. Spróbuj ponownie.";
+  }
 }
 ?>
